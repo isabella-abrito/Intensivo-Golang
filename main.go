1673996f51d356ec93d338970b3b0102b1d18d47
isabella-abrito <isabella.brito@monks.com>
2025-12-09 16:00:49 -0300

    Commitando em uma nova branch

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // var a string: um jeito de declarar uma variável com tipo explícito
 
@@ -9,16 +11,23 @@ func main() {
 	//variável não pode mudar de tipo, se eu fizer a = 1 vai dar erro!
 
 	b := 3.14
-	c := false 
+	c := false
 	d := `Quero imprimir
 	algo aqui
 		quebrando a linha`
 
 	fmt.Println(a) //Println printa e quebra a linha
-	
-	fmt.Printf("%v\n", b) // Printf é outro jeito de imprimir algo na tela, "%v": está passando o valor de b
-	
-	fmt.Printf("%v\n", c)
-	fmt.Printf("%T\n", d) // %T printa o tipo da variável
 
+	fmt.Printf("%v\n", b) // Printf é outro jeito de imprimir algo na tela, "%v": está passando o valor de b
+
+	fmt.Printf("%v\n", c)
+	fmt.Println(d)
+	fmt.Printf("%T", d) // %T printa o tipo da variável
+
+	fmt.Printf("\n\nValor da soma é: %v", Soma(5, 5))
+
+}
+
+func Soma(a int, b int) int {
+	return a + b
 }
